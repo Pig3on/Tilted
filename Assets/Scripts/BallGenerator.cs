145ecfef567fa08d4d3b948c1f85e1d8b62949f8
“TheZhoon” <marin.pavic@five.agency>
2020-05-30 15:03:01 +0200

    add cinemachine camera

--- Assets/Scripts/BallGenerator.cs
+++ Assets/Scripts/BallGenerator.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class BallGenerator : MonoBehaviour
 {
     public GameObject ballPrefab = null;
+
+    public CinemachineFreeLook vCamera;
+
     // Start is called before the first frame update
     void Start()
     {
         createBall();
+
     }
     void createBall(){
-         Instantiate(ballPrefab,this.transform.position,this.transform.rotation);
+        GameObject instance = Instantiate(ballPrefab,this.transform.position,this.transform.rotation);
+        vCamera.Follow = instance.transform;
+        vCamera.LookAt = instance.transform;
     }
 
     // Update is called once per frame
