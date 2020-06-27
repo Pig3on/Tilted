6b284c614dbddb552c107ae505dbe3edd17530fd
“TheZhoon” <marin.pavic@five.agency>
2020-06-27 11:20:24 +0200

    added timeline cinemachine

---
+++ Assets/DirectorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class DirectorController : MonoBehaviour
+{
+    public Level level;
+    private PlayableDirector director;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!level.demoMode)
+        {
+            director = this.GetComponent<PlayableDirector>();
+            director.Play();
+        }
+      
+    }
+}
---
+++ Assets/ShowcaseDone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowcaseDone : MonoBehaviour
+{
+
+    public Level level;
+    public GameObject cinemachineObject;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        if (!level.demoMode)
+        {
+            cinemachineObject.SetActive(false);
+        }
+    }
+}
